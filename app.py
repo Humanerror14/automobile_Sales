2a1350d38b1d9ac47e1ecc5165b1d2ac5bfd25b9
ulil's <aalilamri@gmail.com>
2025-07-04 21:58:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Railway akan set PORT env
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 
     
